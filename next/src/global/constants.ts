db0a8e4a0fb49905075b61cd3893a3d3a82f1c50
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-08-22 19:02:00 +0200

    faet: initial next setup

---
+++ next/src/global/constants.ts
@@ -0,0 +1,11 @@
+/**
+ * Global declaration of domain name of the website. Be aware of the protocol and www or non-www prefix.
+ * @constant
+ */
+export const DOMAIN: string = 'https://mindsoulhealer.pl';
+
+/**
+ * Global declaration of page language.
+ * @constant
+ */
+export const LOCALE: string = 'pl';
---
+++ next/src/global/fonts.ts
@@ -0,0 +1,8 @@
+import { Gentium_Plus } from 'next/font/google';
+
+export const GentiumPlus = Gentium_Plus({
+  subsets: ['latin'],
+  fallback: ['sans-serif'],
+  display: 'swap',
+  weight: ['400', '700'],
+});
