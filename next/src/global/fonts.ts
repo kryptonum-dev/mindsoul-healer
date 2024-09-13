08ee3ff7dcaeff4056718f91b17eb65351aef592
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-09-13 17:29:17 +0200

    feat: update fotns

--- next/src/global/fonts.ts
+++ next/src/global/fonts.ts
@@ -4,11 +4,14 @@ export const Gentium = localFont({
   src: [
     { path: '../../public/fonts/GentiumPlus-Regular.woff2', weight: '400' },
     { path: '../../public/fonts/GentiumPlus-Bold.woff2', weight: '700' },
-    { path: '../../public/fonts/GentiumPlus-Regular.woff', weight: '400' },
-    { path: '../../public/fonts/GentiumPlus-Bold.woff', weight: '700' },
-    { path: '../../public/fonts/GentiumPlus-Regular.ttf', weight: '400' },
-    { path: '../../public/fonts/GentiumPlus-Bold.ttf', weight: '700' },
+  ],
+  fallback: [
+    '../../public/fonts/GentiumPlus-Regular.woff',
+    '../../public/fonts/GentiumPlus-Bold.woff',
+    '../../public/fonts/GentiumPlus-Regular.ttf',
+    '../../public/fonts/GentiumPlus-Bold.ttf',
   ],
   adjustFontFallback: 'Times New Roman',
   variable: '--font-gentium',
+  display: 'swap',
 });
