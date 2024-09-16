7cb160945691b1497573c036f71123e394ca4fdd
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-09-16 08:21:42 +0200

    fix: fix api folder position

--- next/src/utils/sanity.fetch.ts
+++ next/src/utils/sanity.fetch.ts
@@ -37,7 +37,7 @@ export default async function sanityFetch<QueryResponse>({
           cache: 'reload',
         }
       : {
-          ...(isPreviewDeployment || !tags
+          ...(!tags
             ? {
                 cache: 'no-cache',
               }
