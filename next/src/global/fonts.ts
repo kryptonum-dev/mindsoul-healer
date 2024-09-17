ccc10300d9ad54fd95ae0c26ae3892fb83d33e4b
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-09-17 17:14:30 +0200

    feat: move fonts to assets folder

--- next/src/global/fonts.ts
+++ next/src/global/fonts.ts
@@ -2,8 +2,8 @@ import localFont from 'next/font/local';
 
 export const Gentium = localFont({
   src: [
-    { path: '../../public/fonts/GentiumPlus-Regular.woff2', weight: '400' },
-    { path: '../../public/fonts/GentiumPlus-Bold.woff2', weight: '700' },
+    { path: '../../assets/fonts/GentiumPlus-Regular.woff2', weight: '400' },
+    { path: '../../assets/fonts/GentiumPlus-Bold.woff2', weight: '700' },
   ],
   variable: '--font-gentium',
   display: 'swap',
