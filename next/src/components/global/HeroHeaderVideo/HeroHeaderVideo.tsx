9f58e5673f7c81781bb249dc671b725264994ad7
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-09-17 16:19:00 +0200

    fix: remove header and footer for test

--- next/src/components/global/HeroHeaderVideo/HeroHeaderVideo.tsx
+++ next/src/components/global/HeroHeaderVideo/HeroHeaderVideo.tsx
@@ -15,8 +15,6 @@ export default function HeroHeaderVideo({
   authorName,
   index,
 }: HeroHeaderVideoTypes) {
-  return null;
-
   return (
     <section className={styles.section}>
       <div className='max-width'>
